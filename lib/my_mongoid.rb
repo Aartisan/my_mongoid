a4a6fc1f70946318487f20a6edcf8a367d99cf54
Ray <ray.artisan@gmail.com>
2014-02-25 23:48:51 +0800

    Declare a model

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -1,5 +1,24 @@
 require "my_mongoid/version"
 
 module MyMongoid
-  # Your code goes here...
+  module Document
+    def self.included(klass)
+      MyMongoid.regiest_models(klass)
+      klass.extend(ClassMethods)
+    end
+
+    module ClassMethods
+      def is_mongoid_model?
+        true
+      end
+    end
+  end
+
+  def self.models
+    @models ||= []
+  end
+
+  def self.regiest_models(klass)
+    models.push klass unless models.include?(klass)
+  end
 end
