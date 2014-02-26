62988b5c8083c39f22bd8ed3fe957de0d9e21b47
Ray <ray.artisan@gmail.com>
2014-02-26 17:28:30 +0800

    Model attributes and new

--- lib/my_mongoid.rb
+++ lib/my_mongoid.rb
@@ -1,18 +1,6 @@
 require "my_mongoid/version"
 
 module MyMongoid
-  module Document
-    def self.included(klass)
-      MyMongoid.regiest_models(klass)
-      klass.extend(ClassMethods)
-    end
-
-    module ClassMethods
-      def is_mongoid_model?
-        true
-      end
-    end
-  end
 
   def self.models
     @models ||= []
@@ -22,3 +10,53 @@ module MyMongoid
     models.push klass unless models.include?(klass)
   end
 end
+
+module MyMongoid::Document
+  def self.included(klass)
+    MyMongoid.regiest_models(klass)
+    klass.extend(ClassMethods)
+  end
+
+  attr_reader :attributes
+  def initialize(attr)
+    raise ArgumentError, "Argument is not Hash" unless attr.is_a?(Hash)
+    @attributes = attr
+    #process_attributes(attr)
+  end
+
+  def read_attribute(name)
+    attributes[name]
+  end
+
+  def write_attribute(name, value)
+    attributes[name] = value
+  end
+
+  def attributes
+    @attributes ||= {}
+  end
+
+  def new_record?
+    true
+  end
+
+  def process_attributes(attr=nil)
+    attr.each_pair do |key, value|
+      #process_attribute(key, value)
+      send("#{key}=", value)
+    end
+  end
+
+  private
+  def process_attribute(name, value)
+    #responds = respond_to?("#{name}=")
+    send("#{name}", value)
+  end
+end
+
+module MyMongoid::Document::ClassMethods
+  def is_mongoid_model?
+    true
+  end
+end
+
